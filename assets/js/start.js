c306b6c4330caa66621bee8c543bcfe7ee884c3c
HungDo <hung.do@saigontechnology.com>
2019-11-07 16:43:50 +0700

    Update Mobile

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,16 +1,37 @@
+var AURA = AURA || {};
+var showMenu = false;
 $(document).ready(function () {
-    window.onscroll = function (e)
-    {
-        var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight;
-        var docheight = $(document).height();
-        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        var trackLength = docheight - winheight;
-        var pctScrolled = Math.floor(scrollTop / trackLength * 100);
-        if(pctScrolled >0){
-            //$(".navbar").addClass('stuck');
-        }
-        else{
-            $(".navbar").removeClass('stuck');
-        }
-    }
+    AURA.openNav();
+    AURA.closeNav();
+
 })
+
+AURA.openNav = function () {
+    $("body").delegate(".navbar-toggler-icon", "click", function () {
+        $("#mySidenav").fadeIn(500);
+        $("body").addClass('no-scroll');        
+        showMenu = true;
+    });
+}
+
+AURA.closeNav = function () {
+    $("body").delegate(".closebtn", "click", function () {
+        $("#mySidenav").fadeOut(500);
+        $("body").removeClass('no-scroll');        
+        showMenu = false;
+    });
+}
+
+window.onscroll = function (e)
+{
+    var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight;
+    var docheight = $(document).height();
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var trackLength = docheight - winheight;
+    var pctScrolled = Math.floor(scrollTop / trackLength * 100);
+    if (pctScrolled > 0) {
+        //$(".navbar").addClass('stuck');
+    } else {
+        $(".navbar").removeClass('stuck');
+    }
+}
